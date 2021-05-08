e226bb193ee90545f61a3b3d304a6cd40a38e4ae
Zahid Nazir <zahidnazir14@gmail.com>
2021-05-08 14:31:38 +0500

    Unit Test Added

--- Homework-iOSTests/Homework_iOSTests.swift
+++ Homework-iOSTests/Homework_iOSTests.swift
@@ -10,6 +10,19 @@ import XCTest
 
 class Homework_iOSTests: XCTestCase {
 
+    var urlSesion: URLSession!
+
+    
+    override func setUp() {
+        super.setUp()
+        urlSesion = URLSession(configuration: .default)
+    }
+
+    override func tearDown() {
+        urlSesion = nil
+        super.tearDown()
+    }
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -30,4 +43,33 @@ class Homework_iOSTests: XCTestCase {
         }
     }
 
+    
+    func testValidCallToJSONPlaceholer() {
+           // given
+           let url = URL(string: urlToApi)
+
+           //expected
+           let promise = expectation(description: "Status code: 200 or 201")
+
+           // when
+           let dataTask = urlSesion.dataTask(with: url!) { data, response, error in
+
+               // then
+               if let error = error {
+                   XCTFail("Error: \(error.localizedDescription)")
+                   return
+               } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+
+                   if statusCode == 200 || statusCode == 201 {
+                       promise.fulfill()
+                   } else {
+                       XCTFail("Status code: \(statusCode)")
+                   }
+               }
+           }
+           dataTask.resume()
+
+           //Keeps the test running until all expectations are fulfilled, or the timeout interval ends, whichever happens first
+           wait(for: [promise], timeout: 5)
+       }
 }
